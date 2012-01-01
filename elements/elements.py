5bf9fd255110125c89c76f39e26b4100d74be478
Michael <mwelsmandinelle@gmail.com>
2012-01-01 12:10:46 -0800

    updated elements() to return a list of all possible spellings with given symbols

--- elements/elements.py
+++ elements/elements.py
@@ -10,34 +10,19 @@ for line in fileinput.input(FILENAME):
     symbols.append(s)
     slower.append(s.lower())
 
+# can call this as elements("xenon", symbols) for example to use the chemical symbols
 
-# This doesn't properly handle cases where there are multiple solutions
-# for example, "sc" can be Sc (scandium) or SC (sulfur + carbon)
-
-# To fix this I will need to build sets of results to recurse on
-
-def elements(word, soFar = ""):
-     s = word.lower()
-     
-     for e in slower:
-             if s.find(e) == 0:
-                     return elements(s.replace(e, "", 1), soFar + e.capitalize())
-     
-     return soFar
-
-
-
-def elemt(word, soFar = ""):
-    """Produces a tree of chemical symbols representing the possibilities for spelling the given word."""
+def elements(word, symbols, soFar = ""):
+    """Produces a tree of symbols representing the possibilities for spelling the given word."""
+    
+    if len(word) == 0: return [soFar]
     
     result = []
     
     s = word.lower()
     
-    for e in slower:
-        if s.find(e) == 0:
-            result.append(e.capitalize())
-            rest = elemt(s.replace(e, "", 1), soFar + e.capitalize())
-            if rest: result.append(rest)
+    for e in symbols:
+        if s.find(e.lower()) == 0:
+            result += elemt2(s.replace(e.lower(), "", 1), soFar + e)
     
     return result
