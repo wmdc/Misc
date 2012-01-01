3637da2584a621f224ae1cb266376759b36ca222
Michael <mwelsmandinelle@gmail.com>
2012-01-01 12:29:27 -0800

    corrected elements() function

--- elements/elements.py
+++ elements/elements.py
@@ -2,27 +2,21 @@ import fileinput
 
 FILENAME = "elements.txt"
 
-symbols = []
-slower = []
+chem_symbols = []
 
 for line in fileinput.input(FILENAME):
-    s = line.split("\t")[1]
-    symbols.append(s)
-    slower.append(s.lower())
+    chem_symbols.append(line.split("\t")[1])
 
-# can call this as elements("xenon", symbols) for example to use the chemical symbols
+# can call this as elements("xenon", chem_symbols) for example to use the chemical symbols
 
 def elements(word, symbols, soFar = ""):
     """Produces a tree of symbols representing the possibilities for spelling the given word."""
-    
     if len(word) == 0: return [soFar]
     
     result = []
     
-    s = word.lower()
-    
     for e in symbols:
-        if s.find(e.lower()) == 0:
-            result += elemt2(s.replace(e.lower(), "", 1), soFar + e)
+        if word.lower().find(e.lower()) == 0:
+            result += elements(word[len(e):], symbols, soFar + e)
     
     return result
